c437088d7721ccea41f47707e3eb1c9b418a0e92
dpack78 <touchofpianodp@gmail.com>
2017-09-21 17:48:40 -0600

    fix the db stuff

---
+++ lib/DB.class.php
@@ -0,0 +1,40 @@
+<?php
+class DB extends DBConnection{
+	public $link;
+	public function __construct(){
+		parent::__construct();
+		$this->link = $this->getInstance();
+	}
+	
+	public function getRows($query,$a_q){
+		$call = $this->link->prepare($query);
+		$call->execute($a_q);
+		$a_return = [];
+		while(($thisRow = $call->fetch(PDO::FETCH_ASSOC)) !== false){
+			$a_return[] = $thisRow;
+		}
+		return $a_return;
+	}
+	
+	public function getRow($query,$a_q){
+		$call = $this->link->prepare($query);
+		$call->execute($a_q);
+		return $call->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	public function getCol($query,$a_q,$colName){
+		$call = $this->link->prepare($query);
+		$call->execute($a_q);
+		$return = $call->fetch(PDO::FETCH_ASSOC);
+		return $return[$colName];
+	}
+	
+	public function query($query,$a_q){
+		$call = $this->link->prepare($query);
+		$call->execute($a_q);
+	}
+	
+	public function lastID(){
+		return $this->link->lastInsertId();
+	}
+}
